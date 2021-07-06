4a0d53a7c310574c299bbb14afcfa1b028d0efb8
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2021-07-06 14:53:52 +0200

    Fixed bug code and state from fbcontroller

--- oauth-client/Controllers/FacebookController.php
+++ oauth-client/Controllers/FacebookController.php
@@ -5,18 +5,14 @@ namespace App\Controller;
 
 class FacebookController extends ProviderController{
 
-	private $code = "60a3919d567a1";
-	private $state = "fdzefzefze";
-
 	public function handleSuccess(): void
 	{
-	    $_GET["state"] = $this->state ;
-	    $_GET["code"] = $this->code;
-	    if ($this->state !== STATE) {
-	        throw new \RuntimeException("{$this->state}: invalid state");
+	    ["state" => $state, "code" => $code] = $_GET; 
+	    if ($state !== STATE) {
+	        throw new \RuntimeException("{$state}: invalid state");
 	    }
 	    // https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
-	    $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$this->code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
+	    $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
 	    $result = file_get_contents($url);
 	    $resultDecoded = json_decode($result, true);
 	    ["access_token"=> $token] = $resultDecoded;
@@ -28,5 +24,5 @@ class FacebookController extends ProviderController{
 	    ]);
 	    echo file_get_contents($userUrl, false, $context);
 	}
-	
+
 }
